efea0f6be949140f5224a6057dd1c47cc22195b5
arsonaj27 <lamsalashutosh12@gmail.com>
2025-12-15 13:39:56 -0600

    new website

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,6 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = "Astro Blog";
-export const SITE_DESCRIPTION = "Welcome to my website!";
+export const SITE_TITLE = "Ashutosh Lamsal";
+export const SITE_DESCRIPTION =
+	"Geospatial + hydrology researcher building reproducible remote-sensing & ML workflows for flood risk and humanitarian response.";
